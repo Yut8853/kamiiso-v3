f007e31dc65a540e6b872759d890219ae6bff251
Yutaka Kizaki <factory0611@gmail.com>
2026-08-09 20:44:17 +0900

    修正

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: ['sb-49k3c7l4leb4.vercel.run'],
+    allowedHosts: true,
   },
   build: {
     outDir: 'dist',
